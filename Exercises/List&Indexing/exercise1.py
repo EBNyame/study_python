63d32254238d13f7d621bf10fd5ebafceaaf2219
ebnyame <ebnyame001@st.ug.edu.gh>
2025-01-18 05:15:08 +0000

    Slicing

---
+++ Exercises/List&Indexing/exercise1.py
@@ -0,0 +1,9 @@
+favorite_books = ["The Diary of A CEO", "Power of Money", "Rich Dad Poor Dad", "The Hunter", "Young and Powerful"]
+
+print(favorite_books[1])
+favorite_books[-1] = "Young Rich"
+print(favorite_books)
+favorite_books.append("Shall Pass")
+print(favorite_books)
+Slice_first_3 = favorite_books[:3]
+print(Slice_first_3)
